c3d62dd12248a7f6b337ecb1903ab7478c694ce4
Mark <mark.j.wee@gmail.com>
2021-12-08 20:55:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 function scuberGreetingForFeet(rideLength){
-  if rideLength <= 400
-    return `This one is on me!`
-  else if rideLength < 2500 && rideLength > 2000
+  if (rideLength <= 400) {
+    return `This one is on me!`}
+  else if (rideLength < 2500 && rideLength > 2000)
     return `I will gladly take your thirty bucks.`
+  else if (rideLength > 2500)
+    return `No can do.`
 }
 
-function ternaryCheckCity(){
-  
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous"
+      responseTo = 'Thank you so much.'
+      break;
+    case 'not as generous'
+      response = 'Thank you.'
+      break;
+    case 'thanks for everything'
+      response = 'Bye.'
+      break;
+    return response
+  }
 }
+
+  function switchOnCharmFromTip(tip){
+    switch(tip) {
+      case "generous"
+        return 'Thank you so much.'
+
+      case 'not as generous'
+        return 'Thank you.'
+
+      case 'thanks for everything'
+        return 'Bye.'
+
+    }
+  
