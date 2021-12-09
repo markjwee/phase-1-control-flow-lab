6e09389eaafb1d027493fc7797cdbb2166537c4d
Mark <mark.j.wee@gmail.com>
2021-12-08 20:29:29 -0500

    commit tree 9009534b5e82add1aba5a5cc8a46c04b028091a2

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function scuberGreetingForFeet(rideLength){
-  if rideLength <= 400
-    return `This one is on me!`
-  else if rideLength < 2500 && rideLength > 2000
+  if (rideLength <= 400) {
+    return `This one is on me!`}
+  else if (rideLength < 2500 && rideLength > 2000)
     return `I will gladly take your thirty bucks.`
+  else if (rideLength > 2500)
+    return `No can do.`
 }
 
-function ternaryCheckCity(){
-  
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
