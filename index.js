5284f180f390ba70478574470892db5acbbdd93e
Mark <mark.j.wee@gmail.com>
2021-12-08 19:39:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if rideLength <= 400
+    return `This one is on me!`
+  else if rideLength < 2500 && rideLength > 2000
+    return `I will gladly take your thirty bucks.`
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
